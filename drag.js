180daf50236e14b7b2a1c1e8cc23565bbe50cd5a
hww888 <1803771443@qq.com>
2019-03-08 20:34:37 +0800

    chaged new1 drag.js

--- drag.js
+++ drag.js
@@ -1,9 +1,10 @@
 function drag(id){
     var obj = document.getElementById('id');
-    var disX = 0;
-    var disY = 0;
+    //var disX = 0;
+    //var disY = 0;
     obj.onmousedown = function(){
     	disX = ev.pageX-obj.offsetLeft;
     	disY = ev.pageY-obj.gffsetTop;
+    	console.log(123);
     }
 }
